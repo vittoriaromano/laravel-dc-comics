57b8d41fabeada1e05ade4fc78c854f4c377c59f
vittoriaromano <vittoria5005@gmail.com>
2023-05-11 01:08:34 +0400

    PageController Creation

--- database/migrations/2023_05_10_165826_create__comics_table.php
+++ database/migrations/2023_05_10_165826_create__comics_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string ('title', 50);
-            $table->text ('description');
-            $table->string ('thumb');
-            $table->float('price', 4, 2, true);
-            $table->string('series',30);
+            $table->string('title', 50);
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price');
+            $table->string('series');
             $table->date('sale_date');
-            $table->string ('type', 50);
+            $table->string('type');
             $table->text('artists');
             $table->text('writers');
             $table->timestamps();
--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,8 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Comic;
-
-class ComicsTableSeeder extends Seeder
+class ComicSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,24 +14,19 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        $comics = config('comics');
+        $comics = config('comics'); //read comics.php file with config
 
-        foreach($comics as $comic) {
-
+        foreach($comics as $comic){
             $newComic = new Comic();
 
             $newComic->title = $comic['title'];
-            $newComic->decription = $comic['description'];
+            $newComic->description = $comic['description'];
             $newComic->thumb = $comic['thumb'];
             $newComic->price = $comic['price'];
             $newComic->series = $comic['series'];
             $newComic->sale_date = $comic['sale_date'];
-            $newComic->artists =($comic['artists']);
-            $newComic->writers = ($comic['writers']);
+            $newComic->type = $comic['type'];
             $newComic->save();
-        }
     }
 }
-
-
-
+}
