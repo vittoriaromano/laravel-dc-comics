572bad2e46f008782f95c06973537df1b985e708
vittoriaromano <vittoria5005@gmail.com>
2023-05-11 20:41:24 +0400

    Comic Delete

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -90,8 +90,10 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Comic $comic)
     {
-        //
+        $comic->delete();
+        return to_route('comics.index');
+
     }
 }
